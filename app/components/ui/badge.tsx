17ca4f134fc5cf1d2122877f9678def1d8d84c5b
Daychtwoseven <chardrichescol@gmail.com>
2026-03-22 19:41:23 +0800

    c

--- app/components/ui/badge.tsx
+++ app/components/ui/badge.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { ReactNode } from 'react';
 
 export function Badge({ children }: { children: ReactNode }) {
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Badge } from "./components/ui/badge";
 import { ProjectCarousel } from "./components/ui/carousel";
 
